03edc9c31d35b469a8c860f6eba10a9a345306f9
Skiddzie <aidanthe1@gmail.com>
2022-07-27 00:15:07 -0400

    use cases or something

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import coverImage from "../../";
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
   return (
     <section className="my-5">
         <h1 id="about">im aidan!</h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
 }
